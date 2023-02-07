b6500c424aae08f6ce7136146293a1dc21c18484
Weedshaker <weedshaker@gmail.com>
2023-02-07 11:43:27 +0100

    dependencies updated

--- src/es/P2ptProvider.js
+++ src/es/P2ptProvider.js
@@ -7,6 +7,7 @@ import * as decoding from '../y-webrtc/node_modules/lib0/decoding.js'
 import * as math from '../y-webrtc/node_modules/lib0/math.js'
 import * as random from '../y-webrtc/node_modules/lib0/random.js'
 import * as awarenessProtocol from '../y-webrtc/node_modules/y-protocols/awareness.js'
+import * as syncProtocol from '../y-webrtc/node_modules/y-protocols/sync.js'
 
 /**
  * P2ptProvider
@@ -32,7 +33,7 @@ export class P2ptProvider {
   ) {
     this.awareness = awareness;
     this.p2pt = new P2PT(signaling, roomName)
-    console.log('constructor', {roomName, doc, signaling, password, awareness, P2PT: this.p2pt, Y});
+    console.log('constructor', {roomName, doc, signaling, password, awareness, P2PT: this.p2pt, Y, syncProtocol, decoding, encoding});
 
       
     // https://github.com/subins2000/p2pt/blob/master/api-docs.md#new-p2ptannounceurls---identifierstring--
